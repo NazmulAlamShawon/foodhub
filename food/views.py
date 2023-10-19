3e8e13ceb4c76c65bc2d2e40c997c63959831b17
Nazmul Alam <shawonkst15@gmail.com>
2023-10-19 22:27:00 +0600

    add all file

---
+++ food/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+def index(request):
+  item_list = Item.objects.all()
+  
+  context ={
+         'item_list': item_list,
+   }
+  return render(request,'food/index.html', context)
+
+def item(request):
+  return HttpResponse('Hello item!')
+
+def detail(request,item_id):
+  item = Item.objects.get(pk=item_id)
+  context ={
+    'item':item
+  }
+  return render(request,'food/detail.html', context)
