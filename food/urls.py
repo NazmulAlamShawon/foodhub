7e06801bca67ac2c98257f363a5dee23f699d81a
Nazmul Alam <shawonkst15@gmail.com>
2023-10-21 08:20:08 +0600

    add file

--- food/urls.py
+++ food/urls.py
@@ -1,10 +1,13 @@
 from . import views
 from django.urls import path
 
+app_name = 'food'
 urlpatterns = [
   #/food/
     path('', views.index , name="index"),
     #/food/1
     path('<int:item_id>/', views.detail,name="detail"),
     path('item/', views.item , name="item"),
+    #add items to
+    path('add',views.create_item,name="add"),
 ]
